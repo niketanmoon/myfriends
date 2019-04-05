00b31224c7bf198d7fe89c6b9f00f586b00a2cb0
Niketan E. Moon <niketanmoon@gmail.com>
2019-04-05 12:51:13 +0530

    pushing a react app

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+{
+	id:1,
+	name: 'Abhiyan Humane',
+	username: 'abhiyanbhau',
+	email:'ashumane@gmail.com'
+
+},
+{
+	id:2,
+	name: 'Niketan Moon',
+	username: 'niketan.moon',
+	email:'niketanmoon@gmail.com'
+
+},
+{
+	id:3,
+	name: 'Aishwarya Ghirnikar',
+	username: 'ashleyyo',
+	email:'ashleyyo@gmail.com'
+},
+{
+	id:4,
+	name:'Ashish Shende',
+	username: 'ashish',
+	email: 'ashish.shende25@gmail.com'
+},
+{
+	id:5,
+	name: 'Kunal Patil',
+	username: 'kunalpatil',
+	email: 'kunalmpatil94@gmail.com'
+},
+{
+	id:6,
+	name: 'Priyanka Dahilekar',
+	username: 'priyankad',
+	email: 'dahilekarpriyanka169@gmail.com'
+},
+{
+	id:7,
+	name:'Kartik Shende',
+	username: 'kartikx',
+	email:'kartikshende2@gmail.com'
+},
+{
+	id:8,
+	name: 'Rashmi Patil',
+	username: 'rashmip',
+	email: 'rashmipatil196@gmail.com'
+},
+{
+	id:9,
+	name: 'Harshal Shimpi',
+	username: 'harshalshimpi',
+	email: 'harshlashimpi28@gmail.com'
+},
+{
+	id:10,
+	name: 'Shyanki Sakhare',
+	username: 'shyankis',
+	email:'sakhareshyanki@gmail.com'
+
+},
+]
